7f844613a8840139bea00bc33ca05c262baf92ab
yogita-github <103029645+yogita-github@users.noreply.github.com>
2023-03-30 10:31:32 +0530

    gyjsagdu

--- main.py
+++ main.py
@@ -166,13 +166,16 @@ def faqs():
 @app.route('/log',methods=['POST','GET'])
 def log():
     form = LoginForm()
+    print(form.username.data)
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
+        
         if user:
             if bcrypt.check_password_hash(user.password, form.password.data):
                 login_user(user)
                 return redirect(url_for('dashboard'))
     return render_template('log.html',form=form)
+
 
 @app.route('/logout',methods=['GET','POST'])
 @login_required
