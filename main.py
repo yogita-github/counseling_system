cbd331cbf7b11935d7a7704c383e013c370ffb38
yogita-github <103029645+yogita-github@users.noreply.github.com>
2023-03-31 17:25:53 +0530

    gerg

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
 from flask import Flask, render_template, url_for ,request,redirect
-# from flask_sqlalchemy import SQLAlchemy
-# from flask_login import UserMixin, login_user,LoginManager,login_required,logout_user,current_user
-# from flask_wtf import FlaskForm
-# from wtforms import StringField, PasswordField, SubmitField
-# from wtforms.validators import InputRequired, Length, ValidationError
-# from flask_bcrypt import Bcrypt
 import numpy as np
 import pickle
 from sklearn.preprocessing import StandardScaler
@@ -17,50 +11,6 @@ from datetime import date
 import pandas as pd
 
 app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-# db = SQLAlchemy(app)
-# bcrypt = Bcrypt(app)
-# app.config['SECRET_KEY'] = 'thisisasecretkey'
-
-# login_manager = LoginManager()
-# login_manager.init_app(app)
-# login_manager.login_view = "login"
-
-# @login_manager.user_loader
-# def load_user(user_id):
-#     return User.query.get(int(user_id))
-
-
-# class User(db.Model, UserMixin):
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(20), nullable=False,unique=True)
-#     password = db.Column(db.String(80), nullable=False)
-
-# class RegistrationForm(FlaskForm):
-#     username = StringField(validators=[InputRequired(), Length(
-#         min=4, max=20)], render_kw={"placeholder":"Username"})
-    
-#     password = PasswordField(validators=[InputRequired(),Length(
-#         min=4 , max=20)], render_kw={"placeholder":"Password"})
-    
-#     submit = SubmitField('Register')
-
-#     def validate_username(self,username):
-#         existing_user_username = User.query.filter_by(
-#             username=username.data).first()
-        
-#         if existing_user_username:
-#             raise ValidationError(
-#                 'That username already exists.Please choose a different one.')
-        
-# class LoginForm(FlaskForm):
-#     username = StringField(validators=[InputRequired(), Length(
-#         min=4, max=20)], render_kw={"placeholder":"Username"})
-    
-#     password = PasswordField(validators=[InputRequired(),Length(
-#         min=4 , max=20)], render_kw={"placeholder":"Password"})
-    
-#     submit = SubmitField("Login")
 
 
 @app.route('/')
@@ -82,7 +32,6 @@ def forms():
     town = request.form['town']
     pin = request.form['pin']
     country = request.form['country']
-    dob = request.form['dob']
     vitals = [age, sbp, hbp, h_rate, glc, spo2, temp_c]
     res = predict(gender,age, sbp, hbp, h_rate, glc, spo2, temp)
     if (res[0] == 1):
@@ -120,7 +69,7 @@ def forms():
         g = 'female'
     elif(gender == '0'):
         g='male'
-    return render_template('report.html', res = result, zipped_data = zip(vitals, datalist.vals), data = data, phy = phy, meds = meds, medi = medi, nutri=nutri, syms=syms, name = name, pin = pin, country = country, dob = dob, gender = g, town = town)
+    return render_template('report.html', res = result, zipped_data = zip(vitals, datalist.vals), data = data, phy = phy, meds = meds, medi = medi, nutri=nutri, syms=syms, name = name, pin = pin, country = country, gender = g, town = town)
 
 @app.route('/forms', methods=['POST', 'GET'])
 def form():
@@ -133,7 +82,7 @@ def rec(stri):
     nutris = file.nutris[stri]
     meds = file.meds[stri]
     syms = file.syms[stri] #this line is added
-    return parsed, physical, meditation, nutris, meds, syms #syms is added
+    return parsed, physical, meditation, nutris, meds, syms 
 
 @app.route('/tips', methods=['POST', 'GET'])
 def tips():
@@ -174,52 +123,6 @@ def faqs():
     return render_template('faq.html')
 
 
-# @app.route('/log',methods=['POST','GET'])
-# def log():
-#     form = LoginForm()
-#     print(form.username.data)
-#     if form.validate_on_submit():
-#         user = User.query.filter_by(username=form.username.data).first()
-        
-#         if user:
-#             if bcrypt.check_password_hash(user.password, form.password.data):
-#                 login_user(user)
-#                 return redirect(url_for('dashboard'))
-#     return render_template('log.html',form=form)
-
-
-# @app.route('/logout',methods=['GET','POST'])
-# @login_required
-# def logout():
-#     logout_user()
-#     return redirect(url_for('logi'))
-
-# @app.route('/register', methods=['POST','GET'])
-# def register():
-#     form = RegistrationForm()
-
-#     if form.validate_on_submit():
-#         hashed_password = bcrypt.generate_password_hash(form.password.data)
-#         new_user = User(username=form.username.data, password=hashed_password)
-#         db.session.add(new_user)
-#         db.session.commit()
-#         return redirect(url_for('logi'))
-
-#     return render_template('register.html', form=form)
-
-# @app.route('/dashboard',methods=['POST','GET'])
-# @login_required
-# def dashboard():
-#     return render_template('dashboard.html')
-
-#     if form.validate_on_submit():
-#       hashed_password = bcrypt.generate_password_hash(form.password.data)
-#       new_user = User(username=form.username.data, password=hashed_password)
-#       db.session.add(new_user)
-#       db.session.commit()
-#       return redirect(url_for('log'))
-
-#     return render_template('register.html',form=form)
 
 def predict(g,a,s,h,hr,gl,sp,temp):
     p = np.array([[g,a,s,h,hr,gl,sp,temp]])
